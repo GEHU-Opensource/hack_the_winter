08b4bee7827c7641e6a734858cefb9594667aa01
TechLead-War <ayushkapri.richard@gmail.com>
2026-01-19 23:44:53 +0530

    Adding statements.

--- src/components/pages/home/ps/statements.ts
+++ src/components/pages/home/ps/statements.ts
@@ -1,74 +1,68 @@
-interface Statement {
+export interface Statement {
     title: string;
     link?: string;
     description?: string;
     isTheme?: boolean;
+    openMode: "iframe" | "external";
 }
 
-const currentStatements: Statement[] = [
+export const currentStatements: Statement[] = [
     {
-        title: "Web / App",
-        description: "Any idea with a Web / App interface",
-        isTheme: true,
+        title: "Business and Startups Themes",
+        link: "https://www.overleaf.com/read/qyvwccbwtzsp#1c5d19",
+        openMode: "external",
     },
     {
-        title: "Fintech",
-        description: "Financial technology solutions",
-        isTheme: true,
+        title: "Core Themes",
+        link: "https://www.overleaf.com/read/rvnbscbncqjs#28c5c0",
+        openMode: "external",
     },
     {
-        title: "AR / VR",
-        description: "Augmented & Virtual Reality innovations",
-        isTheme: true,
+        title: "Governance Themes",
+        link: "https://www.overleaf.com/read/gghbzqrjrktd#a30709",
+        openMode: "external",
     },
     {
-        title: "Governance",
-        description: "Build anything for the Government",
-        isTheme: true,
+        title: "Fintech Themes",
+        link: "https://www.overleaf.com/read/wnpwtvthsrgg#50b660",
+        openMode: "external",
     },
     {
-        title: "Web3 / Blockchain",
-        description: "Decentralized & blockchain solutions",
-        isTheme: true,
+        title: "Foundational Systems Themes (Only 1st year teams)",
+        link: "https://www.overleaf.com/read/kngrqyzzmvtz#12f5e0",
+        openMode: "external",
     },
     {
-        title: "AI / ML",
-        description: "Artificial Intelligence & Machine Learning",
-        isTheme: true,
-    },
-    {
-        title: "IoT & Automation",
-        description: "Internet of Things & automation solutions",
-        isTheme: true,
-    },
-    {
-        title: "Open Innovation",
-        description: "Any innovative idea outside the themes",
-        isTheme: true,
+        title: "Let's do AI Themes",
+        link: "https://www.overleaf.com/read/vmtfbmpgnmym#8ea592",
+        openMode: "external",
     },
 ];
 
-const pastStatements: Statement[] = [
+export const pastStatements: Statement[] = [
     {
         title: "AI CAPTCHA Solver",
         link: "https://docs.google.com/document/d/1WAGW9vfuDpJDFCwbo0tla_n1eNT21JCawRRw7OEapbo/preview",
+        openMode: "iframe",
     },
     {
         title: "Flowchart Maker",
         link: "https://docs.google.com/document/d/1clXvAPFvjHbGQ9VllL7_TkIIOvqPFIxIIStKboi3BbY/preview",
+        openMode: "iframe",
     },
     {
         title: "String Matching",
         link: "https://docs.google.com/document/d/1ocGHfb4R72NqaelWS9AAM-t3tTzuYUnAhSzcZDhrP5M/preview",
+        openMode: "iframe",
     },
     {
         title: "Contact Sharing",
         link: "https://docs.google.com/document/d/1rcRB_D3H2O6oOaDeTmL0tBMxcgE8rzYmqzeogimlLcI/preview",
+        openMode: "iframe",
     },
     {
         title: "Traffic Management",
         link: "https://docs.google.com/document/d/1bFQErWKMvuH0hOcysGD9PIeiPPULy90DVy8xYm5OV1Y/preview",
+        openMode: "iframe",
     },
 ];
-
-export { currentStatements, pastStatements, type Statement };
